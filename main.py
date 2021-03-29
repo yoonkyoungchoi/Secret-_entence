4279f1f14055861cb01c82f95ef1a063cdeba301
yoonkyoungchoi <63581073+yoonkyoungchoi@users.noreply.github.com>
2021-03-30 02:35:38 +0900

    Label 설정

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5.QtWidgets import QApplication, \
-    QWidget, QLabel, QTextEdit, QVBoxLayout, QMainWindow
+    QWidget, QLabel, QTextEdit, QVBoxLayout, QMainWindow, QPushButton
 
 
 class Main(QWidget):
@@ -9,23 +9,30 @@ class Main(QWidget):
         self.Main()
 
     def Main(self):
-        self.lbl1 = QLabel('Enter your sentence:')
-        self.te = QTextEdit()
-        self.te.setAcceptRichText(False)
-        self.lbl2 = QLabel('The number of words is 0')
+        label1 = QLabel('삐빅 삐빅 알 수 없음', self)
+        label1.move(290, 30)
+        font1 = label1.font()
+        font1.setPointSize(20)
+        label1.setFont(font1)
+        label2 = QLabel(' 좋아하는 단어와 문장을 영어로 작성하여',self)
+        label2.move(260, 100)
+        font2 = label2.font()
+        font2.setPointSize(13)
+        label2.setFont(font2)
+        label3 = QLabel('암호문이 된 문장을 확인해보세요!', self)
+        label3.move(290, 130)
+        font3 = label3.font()
+        font3.setPointSize(13)
+        label3.setFont(font3)
 
-        self.te.textChanged.connect(self.text_changed)
+        self.textedit = QTextEdit(self)
 
-        vbox = QVBoxLayout()
-        vbox.addWidget(self.lbl1)
-        vbox.addWidget(self.te)
-        vbox.addWidget(self.lbl2)
-        vbox.addStretch()
-
-        self.setLayout(vbox)
+        btn1 = QPushButton('결과 보기', self)
+        btn1.move(370, 530)
+        btn1.resize(100, 30)
 
         self.setWindowTitle('Secret')
-        self.setGeometry(600, 300, 800, 500)
+        self.setGeometry(600, 300, 800, 600)
         self.show()
 
     def text_changed(self):

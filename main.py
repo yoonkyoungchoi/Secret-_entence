b4ecf53ba62f2546485640e1ccbfd19cb6136958
yoonkyoungchoi <63581073+yoonkyoungchoi@users.noreply.github.com>
2021-04-13 15:35:00 +0900

    결과물 출력 textbox 설정

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ class Main(QWidget):
 
         #문자 입력하는 edit 박스
         self.text1 = QPlainTextEdit(self)
-        self.text1.setGeometry(360, 199, 100, 30) # x, y, w, h
+        self.text1.setGeometry(360, 199, 100, 31) # x, y, w, h
 
         label5 = QLabel('암호화 할 문장 입력', self)
         label5.move(350, 240)
@@ -45,22 +45,43 @@ class Main(QWidget):
         label5.setFont(font5)
 
         self.text2 = QPlainTextEdit(self)
-        self.text2.setGeometry(240, 280, 350, 40)
+        self.text2.setGeometry(245, 270, 350, 40) # x, y, w, h
 
         #결과값 출력 버튼
         self.btn1 = QPushButton('결과 확인하기', self)
         self.btn1.clicked.connect(self.show_result)
-        self.btn1.move(370, 530)
+        self.btn1.move(370, 630)
         self.btn1.resize(100, 30)
+
+        label6 = QLabel('결과 출력', self)
+        label6.move(380, 330)
+        font6 = label6.font()
+        font6.setPointSize(10)
+        label6.setFont(font6)
 
         #타이틀 메뉴
         self.setWindowTitle('Secret')
-        self.setGeometry(600, 300, 800, 600)
+        self.setGeometry(600, 300, 800, 700) # x, y, w, h
         self.show()
 
     def show_result(self):
-        keyword = self.text2.toPlainText()
-        result = self.text1.toPlainText()
+        self.answer_secrit = ''
+        self.answer_secrit1 = ''
+        self.keyword = self.text2.toPlainText()
+        self.result = self.text1.toPlainText()
+        for line in self.keword():
+            self.answer_secrit += line
+
+        for line2 in self.result():
+            self.answer_secrit += line2
+
+        label7 = QLabel(" ", self)
+        label7.move(290, 400)
+        font7 = label7.font()
+        font7.setPointSize(20)
+        label7.setFont(font7)
+
+
 
     def text_changed(self):
         text = self.te.toPlainText()
